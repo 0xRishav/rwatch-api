089c6885e38db43d24c5217b10a71f60c98e3d76
Rishav Bharti <rshvbharti@gmail.com>
2021-05-12 08:56:14 +0530

    setting up heroku

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = 3001;
 app.get("/", (req, res) => {
   return res.json({ success: true, message: "server is up and running" });
 });
+app.get("/about", (req, res) => {
+  return res.json({ success: true, message: "server is up and running" });
+});
 
 app.listen(PORT, (req, res) => {
   console.log(`Express app is running on port: ${PORT}`);
