625070b6c50b0abdb011d8f86228fdeaf957176b
Rishav Bharti <rshvbharti@gmail.com>
2021-08-15 16:40:18 +0530

    updated database

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const uri = "mongodb+srv://rishavbharti:Gm1581ddnagar@rwatch.67eqj.mongodb.net/test";
+const uri = "mongodb+srv://rishavbharti:Gm1581ddnagar@rwatch.67eqj.mongodb.net/RWatch";
 
 mongoose
   .connect(uri, {
